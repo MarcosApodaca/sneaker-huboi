46857554e3ab7a0d2bd27a9d366af011e76de954
Marcos Apodaca <marcosapodaca@outlook.es>
2024-03-07 16:08:17 +0100

    update

---
+++ src/ts/OpcionesDeFiltro.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+;
+export interface Productos {
+  id: number;
+  href: string;
+  img: string;
+  title: string;
+  marca: string;
+  tipo: string;
+  tallas: string;
+  precio: string;
+}
+
+interface OrdenCardProps {
+  productos: Productos;
+}
+
+//Aqui se renderiza los datos de la orden
+
+const OrdenCard: React.FC<OrdenCardProps> = ({ productos }) => {
+  return (
+    <div className="orden-card bg-white/60 w-60 p-3 m-3 rounded-md shadow-md md:w-1/3">
+      <img src={productos.img}/>
+        <p><strong>Profesional:</strong> {productos.title}</p>
+        <p><strong>Precio:</strong> {productos.precio}</p>
+        <p><strong>Fecha:</strong> {productos.tallas}</p>
+        <p className='mt-5'><strong>Descripcion</strong> <br/> {productos.marca}</p>
+    </div>
+  );
+};
+
+export default OrdenCard;
